acc861ff557993c6e8d5cc4adac15f2b471294bd
Gary A. Mort <garyamort@gmail.com>
2013-12-02 17:12:58 -0500

    Modification to core code and GAE configuration to allow simplexml to read local files(???)

--- installation/index.php
+++ installation/index.php
@@ -10,7 +10,7 @@ if (version_compare(PHP_VERSION, '5.3.1', '<'))
 {
 	die('Your host needs to use PHP 5.3.1 or higher to run this version of Joomla!');
 }
-
+libxml_disable_entity_loader(false);
 /**
  * Constant that is checked in included files to prevent direct access.
  * define() is used in the installation folder rather than "const" to not error for PHP 5.2 and lower
